585715a00a8575f2a70e15140ba1902f09ec23b2
Sergey <sproshchenko@yandex.ru>
2022-04-10 03:07:46 +0300

    - added parsing information about players and teams for the game page - added parsing info about time and place

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-
+from modules import DataScrapper
 
 if __name__ == '__main__':
+    data_scrapper = DataScrapper()
+    data_scrapper.main()
+    # data_scrapper.get_table_fields()
+
     print('nba_prediction')
---
+++ modules/setting.py
@@ -0,0 +1,11 @@
+
+# www.basketball-reference.com
+class ScrapperSetting:
+    TEAMS = {}
+    STATS = {'player': 'starters', 'mp': 'minutes', 'fg': 'field_goal', 'fga': 'field_goal_attempts', 'fg3': '3p_made',
+             'fg3a': '3p_attempts', 'ft': 'free_throw_made', 'fta': 'free_throw_attempts', 'orb': 'offensive_rebounds',
+             'drb': 'defensive_rebounds', 'trb': 'total_rebounds',
+             'ast': 'ast', 'stl': 'stl', 'blk': 'blk', 'tov': 'turnovers', 'pf': 'personal_fouls', 'pts': 'points'
+             }
+
+    ALL_STATS = {'player': 'starters', 'mp': 'mp', 'fg': 'fg', 'fga': 'fga', 'fg_pct': 'fg%', 'fg3': '3p', 'fg3a': '3pa', 'fg3_pct': '3p%', 'ft': 'ft', 'fta': 'fta', 'ft_pct': 'ft%', 'orb': 'orb', 'drb': 'drb', 'trb': 'trb', 'ast': 'ast', 'stl': 'stl', 'blk': 'blk', 'tov': 'tov', 'pf': 'pf', 'pts': 'pts', 'plus_minus': '+/-', 'ts_pct': 'ts%', 'efg_pct': 'efg%', 'fg3a_per_fga_pct': '3par', 'fta_per_fga_pct': 'ftr', 'orb_pct': 'orb%', 'drb_pct': 'drb%', 'trb_pct': 'trb%', 'ast_pct': 'ast%', 'stl_pct': 'stl%', 'blk_pct': 'blk%', 'tov_pct': 'tov%', 'usg_pct': 'usg%', 'off_rtg': 'ortg', 'def_rtg': 'drtg', 'bpm': 'bpm'}
